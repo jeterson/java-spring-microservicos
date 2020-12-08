74df28dfb5e6b45bcc64fe42256df63b11b1be3b
Jeterson - 747 <jeterson@cairunet.ad.br>
2020-12-08 16:37:45 -0400

    Configuring zuul api gateway

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -34,12 +34,7 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
-		try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 		logger.info("PORT = " +env.getProperty("local.server.port"));
 		
 		Worker obj = repository.findById(id).get();
