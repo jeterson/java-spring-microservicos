721c0be63b8f6b4d7b314ab75caf11ec4a473b89
Jeterson - 747 <jeterson@cairunet.ad.br>
2020-12-08 16:23:24 -0400

    project hr-api-gateway-zull created

---
+++ hr-api-gateway-zull/src/main/java/com/devsuperior/hreapigatewayzull/HrApiGatewayZullApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hreapigatewayzull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatewayZullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayZullApplication.class, args);
+	}
+
+}
---
+++ hr-api-gateway-zull/src/test/java/com/devsuperior/hreapigatewayzull/HrApiGatewayZullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hreapigatewayzull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
