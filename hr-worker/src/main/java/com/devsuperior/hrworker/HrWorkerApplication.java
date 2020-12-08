26c3aaef5545e3e676db2e9e81bd80f3873272b1
Jeterson - 747 <jeterson@cairunet.ad.br>
2020-12-08 17:55:30 -0400

    hr-worker as a config server client

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -3,7 +3,6 @@ package com.devsuperior.hrworker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
 @EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,10 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 public class WorkerResource {
 
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Value("${test.config}")
+	private String textConfig;
+	
 	@Autowired
 	private Environment env;
 	
@@ -30,6 +35,12 @@ public class WorkerResource {
 	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping("/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG = " + textConfig);
+		return ResponseEntity.noContent().build();
 	}
 	
 	@GetMapping(value = "/{id}")
