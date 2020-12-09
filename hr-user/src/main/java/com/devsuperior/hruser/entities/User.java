dde640d444c6a4b56b201396aec85ce2c67e52ed
Jeterson - 747 <jeterson@cairunet.ad.br>
2020-12-09 09:13:16 -0400

    UserRepository, UserResource Zuul config

--- hr-user/src/main/java/com/devsuperior/hruser/entities/User.java
+++ hr-user/src/main/java/com/devsuperior/hruser/entities/User.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -25,6 +26,7 @@ public class User implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(unique = true)
 	private String name;
 	private String email;
 	private String password;
